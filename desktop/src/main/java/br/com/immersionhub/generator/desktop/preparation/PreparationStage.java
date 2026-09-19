0c01e3ef6d591349ffcf8f6fe5633b8d038b5a3e
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-18 23:13:40 -0300

    feat: expose preparation progress stages (#36)

--- desktop/src/main/java/br/com/immersionhub/generator/desktop/preparation/PreparationStage.java
+++ desktop/src/main/java/br/com/immersionhub/generator/desktop/preparation/PreparationStage.java
@@ -2,7 +2,12 @@ package br.com.immersionhub.generator.desktop.preparation;
 
 public enum PreparationStage {
     PREPARING("Preparando material…"),
+    PREPARING_AUDIO("Preparando áudio…"),
+    TRANSCRIBING("Transcrevendo material…"),
+    TRANSCRIPTION_READY("Transcrição concluída."),
     ALIGNING("Ajustando tempos…"),
+    ALIGNMENT_READY("Tempos ajustados."),
+    USING_BASE_TIMINGS("Usando tempos preparados."),
     READY("Material pronto.");
 
     private final String userMessage;
