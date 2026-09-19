59f526115cb221323a746e2fad55bf38687c6159
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-19 12:17:46 -0300

    feat(#48): add ReviewInvalidationScope.java

---
+++ desktop/src/main/java/br/com/immersionhub/generator/desktop/workspace/ReviewInvalidationScope.java
@@ -0,0 +1,11 @@
+package br.com.immersionhub.generator.desktop.workspace;
+
+public enum ReviewInvalidationScope {
+    CUE_AND_AFFECTED_UNITS,
+    CUE_TIMING_ONLY,
+    UNIT_ONLY,
+    UNIT_TIMING_ONLY,
+    AFFECTED_GROUP_ONLY,
+    SPEAKER_ONLY,
+    WORKSPACE_DEPENDENCY
+}
