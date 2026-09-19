d2e0b3e6503c91cf3aa6826254cb60b6a3e004ed
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-18 23:17:28 -0300

    fix: import atomic move fallback for preview cache (#37)

--- desktop/src/main/java/br/com/immersionhub/generator/desktop/timing/FfmpegMediaProcessor.java
+++ desktop/src/main/java/br/com/immersionhub/generator/desktop/timing/FfmpegMediaProcessor.java
@@ -7,6 +7,7 @@ import br.com.immersionhub.generator.desktop.model.SourceMedia;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.AtomicMoveNotSupportedException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
